79ce2a1ab65c04252e2628287f8ad13e7c12c717
Greg Hope <greg.hope@outlook.com>
2023-12-14 21:14:07 +0000

    Store PGN for game

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import player
 import time
 
 import chess
+import chess.pgn
 
 white = player.player_stockfish(skill=0)
 black = player.player_random()
@@ -11,6 +12,8 @@ black = player.player_random()
 so = SquareOff()
 try:
     so.start_game()
+    game = chess.pgn.Game()
+    node = game
 
     board = chess.Board()
 
@@ -19,10 +22,12 @@ try:
         move = active(board)
         print(move.uci())
         board.push(move)
+        node = node.add_variation(move)
         so.make_move(move)
         active, non_active = non_active, active
         time.sleep(1)
 
     so.end_game()
+    print(game)
 finally:
     so.disconnect()
